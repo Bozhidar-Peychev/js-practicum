e200962de52a1dda6863fc23d54cc2853211b983
Bozhidar Peychev <b.peychev@dimiroma.com>
2020-12-19 22:27:54 +0200

    wip

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -1,121 +1,74 @@
-const calculator = {
-  displayValue: "0",
-  displayHistory: "0",
-  firstOperand: null,
-  waitingForSecondOperand: false,
-  operator: null,
-};
-
-const performCalculation = {
-  "/": (firstOperand, secondOperand) => firstOperand / secondOperand,
-
-  "*": (firstOperand, secondOperand) => firstOperand * secondOperand,
-
-  "+": (firstOperand, secondOperand) => firstOperand + secondOperand,
-
-  "-": (firstOperand, secondOperand) => firstOperand - secondOperand,
-
-  "=": (firstOperand, secondOperand) => secondOperand,
-};
-
-const CalculationHistory = {
-  "/": (firstOperand) => `${firstOperand} / `,
-
-  "*": (firstOperand) => `${firstOperand} * `,
-
-  "+": (firstOperand) => `${firstOperand} + `,
-
-  "-": (firstOperand) => `${firstOperand} - `,
-};
-
-const inputDigit = (digit) => {
-  const { displayValue, waitingForSecondOperand } = calculator;
-
-  if (waitingForSecondOperand === true) {
-    calculator.displayValue = digit;
-    calculator.waitingForSecondOperand = false;
+function getHistory() {
+  return document.getElementById("history-value").innerText;
+}
+function getOutput() {
+  return document.getElementById("output-value").innerText;
+}
+function printHistory(num) {
+  document.getElementById("history-value").innerText = num;
+}
+function printOutput(num) {
+  if (num == "") {
+    document.getElementById("output-value").innerText = num;
   } else {
-    calculator.displayValue =
-      displayValue === "0" ? digit : displayValue + digit;
+    document.getElementById("output-value").innerText = getFormattedNumber(num);
   }
-};
-
-const inputDecimal = (dot) => {
-  if (!calculator.displayValue.includes(dot)) {
-    calculator.displayValue += dot;
+}
+function getFormattedNumber(num) {
+  if (num == "-") {
+    return "";
   }
-};
-
-const handleOperator = (nextOperator) => {
-  const { firstOperand, displayValue, operator } = calculator;
-  const inputValue = parseFloat(displayValue);
-
-  if (operator && calculator.waitingForSecondOperand) {
-    calculator.operator = nextOperator;
-    return;
-  }
-
-  if (firstOperand == null) {
-    calculator.firstOperand = inputValue;
-  } else if (operator) {
-    const currentValue = firstOperand || 0;
-    const result = performCalculation[operator](currentValue, inputValue);
-    const calculationHistory = CalculationHistory[operator](currentValue);
-
-    console.log("test", calculationHistory);
-    calculator.displayValue = String(result);
-    calculator.historyValue = calculationHistory;
-    calculator.firstOperand = result;
-  }
-
-  calculator.waitingForSecondOperand = true;
-  calculator.operator = nextOperator;
-};
-
-const resetCalculator = () => {
-  calculator.displayValue = "0";
-  calculator.firstOperand = null;
-  calculator.waitingForSecondOperand = false;
-  calculator.operator = null;
-  calculator.displayHistory = "0";
-};
-
-const updateDisplay = () => {
-  const display = document.querySelector(".calculator-screen");
-  const displayHistory = document.querySelector(".calculator-history");
-  display.value = calculator.displayValue;
-  displayHistory.value = calculator.displayHistory;
-  console.log("test", calculator.displayHistory);
-};
-
-updateDisplay();
-
-const keys = document.querySelector(".calculator-keys");
-
-keys.addEventListener("click", (event) => {
-  const { target } = event;
-  if (!target.matches("button")) {
-    return;
-  }
-
-  if (target.classList.contains("operator")) {
-    handleOperator(target.value);
-    updateDisplay();
-    return;
-  }
-
-  if (target.classList.contains("decimal")) {
-    inputDecimal(target.value);
-    updateDisplay();
-    return;
-  }
-
-  if (target.classList.contains("all-clear")) {
-    resetCalculator();
-    updateDisplay();
-    return;
-  }
-
-  inputDigit(target.value);
-  updateDisplay();
-});
+  let n = Number(num);
+  let value = n.toLocaleString("en");
+  return value;
+}
+function reverseNumberFormat(num) {
+  return Number(num.replace(/,/g, ""));
+}
+let operator = document.getElementsByClassName("operator");
+for (let i = 0; i < operator.length; i++) {
+  operator[i].addEventListener("click", function () {
+    if (this.id == "clear") {
+      printHistory("");
+      printOutput("");
+    } else if (this.id == "backspace") {
+      var output = reverseNumberFormat(getOutput()).toString();
+      if (output) {
+        //if output has a value
+        output = output.substr(0, output.length - 1);
+        printOutput(output);
+      }
+    } else {
+      let output = getOutput();
+      let history = getHistory();
+      if (output == "" && history != "") {
+        if (isNaN(history[history.length - 1])) {
+          history = history.substr(0, history.length - 1);
+        }
+      }
+      if (output != "" || history != "") {
+        output = output == "" ? output : reverseNumberFormat(output);
+        history = history + output;
+        if (this.id == "=") {
+          let result = eval(history);
+          printOutput(result);
+          printHistory("");
+        } else {
+          history = history + this.id;
+          printHistory(history);
+          printOutput("");
+        }
+      }
+    }
+  });
+}
+let number = document.getElementsByClassName("number");
+for (let i = 0; i < number.length; i++) {
+  number[i].addEventListener("click", function () {
+    let output = reverseNumberFormat(getOutput());
+    if (output != NaN) {
+      output = output + this.id;
+      printOutput(output);
+    }
+  });
+}
